ab3e48025b399bc4045ae4932a346e9d644a3f81
muayad <muayad.nazim@gmail.com>
2024-10-16 09:16:39 +0300

    make the header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { IoSearch } from "react-icons/io5";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3" >
+        <Link to='/'>
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-600">Real</span>
+            <span className="text-slate-800 ">Estate</span>
+        </h1></Link>
+      
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center" >
+            <input type="text" placeholder="Search..." 
+            className="focus:outline-none bg-transparent w-24 sm:w-64" />
+            <IoSearch  className="text-slate-500"/>
+        </form>
+
+        <div className="flex text-slate-500 justify-between gap-x-2.5">
+            <Link className='hidden sm:inline  ' to='/'>Home</Link>
+            <Link className='hidden sm:inline' to='/about'>About</Link>
+            <Link className='' to='/sign-in'>Sign In</Link>
+        </div>
+      </div>
+    </header>
+  )
+}
