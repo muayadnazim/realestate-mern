8120368412bd81b95cafc7daa431ad4d2d0e32c0
muayad <muayad.nazim@gmail.com>
2024-11-26 10:37:44 +0300

    add listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import {conectDb} from './db/connectDb.js'
 // import User from './models/user.model.js'
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use('/api/listing',listingRouter)
 
 app.use((error,req,res,next)=>{
 let stateCode = error.stateCode ||500;
