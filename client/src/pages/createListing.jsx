7c170a638cec898c7175ec4399db75209a15b585
muayad <muayad.nazim@gmail.com>
2024-12-03 11:29:49 +0300

    complete upload listing images functionality

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -1,6 +1,66 @@
+import { supabase } from '../../supabase.js'
+import { useState } from "react"
+
+export default function CreateListing() {
+
+   
+const [imageUploadError,setImageUploadError]=useState(false)
+const [files,setFiles]=useState([])
+const [formData,setFormData]=useState({ imageUrls:[]})
+
+const [uploading, setUploading] = useState(false);
+    
+    
 
 
-export default function createListing() {
+const handleImagesSubmit = async ()=>{
+if (files.length> 0 && files.length + formData.imageUrls.length < 7 ){
+    setUploading(true)
+    setImageUploadError(false)
+const promises = [];
+
+
+for(let i=0;i<files.length;i++){
+    
+    
+    promises.push(storeImage(files[i]))
+  }
+await Promise.all(promises).then((urls)=>{
+setFormData({...formData,imageUrls:formData.imageUrls.concat(urls)})
+setImageUploadError(false)
+setUploading(false);
+}).catch(()=>{
+    setImageUploadError('Image upload failed (2 MB per image)')
+
+})
+}else{
+    setImageUploadError('you can only upload 6 image per listing')
+    setUploading(false);
+}
+}
+
+const storeImage =   (file) => {
+    return new Promise ( (resolve,reject) => {
+    
+        const storeImage =async ()=>{
+            const { data, error } = await supabase.storage.
+        from('profileImage').upload(`${Date.now()+file.name}_image.png`,
+           file); 
+    if (error)  { 
+        reject(error)
+     console.error('Error uploading image:', error); return null;
+    } 
+     const urls =  supabase.storage.from('profileImage').getPublicUrl(data?.path)
+    resolve(urls) }
+    storeImage()
+            })}
+
+const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
 
   return (
     <main className="p-3 max-w-4xl mx-auto">
@@ -134,13 +194,28 @@ required
 
 
 <div className="flex gap-4">
-<input className="p-3 border border-gray-300 rounded w-full" type="file" id='images' accept="image/*" multiple />
-<button className="p-3 text-green-700 border border-green-700 
-rounded uppercase hover:shadow-lg disabled:opacity-80">Upload</button>
+<input onChange={(e)=>setFiles(e.target.files)} className="p-3 border border-gray-300 rounded w-full" type="file" id='images' accept="image/*" multiple />
+<button onClick={ ()=>handleImagesSubmit()} type="button" className="p-3 text-green-700 border border-green-700 
+rounded uppercase hover:shadow-lg disabled:opacity-80">{uploading ? 'Uploading...' : 'Upload'}</button>
 </div>
+<p className='text-red-700'>{imageUploadError&&imageUploadError}</p>
+{
+    formData.imageUrls.length>0 && formData.imageUrls.map((url,index)=>  (
+       
+        
+<div key={url.data.publicUrl} className='flex justify-between p-3 border items-center'>
+    <img   className='w-20 h-20 object-contain rounded-lg' src={url.data.publicUrl} alt="listing image" />
 
+<button
+type='button'
+onClick={() => handleRemoveImage(index)}
+className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'
+> Delete </button>
 
-<button className="p-3 bg-slate-700 text-white 
+</div>
+    ))
+}
+<button  className="p-3 bg-slate-700 text-white 
 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing </button>
 </div>
 
