7ad31bd6770fa6afb240527abb1224f57c8a43b5
muayad <muayad.nazim@gmail.com>
2024-12-17 19:37:49 +0300

    add search api  route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,27 @@
 import { IoSearch } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,13 +32,20 @@ export default function Header() {
           </h1>
         </Link>
 
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             className="focus:outline-none bg-transparent w-24 sm:w-64"
           />
-          <IoSearch className="text-slate-500" />
+          <button>
+            <IoSearch className="text-slate-500" />
+          </button>
         </form>
 
         <div className="flex text-slate-500 justify-between gap-x-2.5">
